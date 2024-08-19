b17b3d7673a61b6c0d8e43a57df31e5c32495b94
Dmitrii Riazantsev <dilix90@gmail.com>
2024-08-19 22:17:45 +0200

    Run on ios

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,11 +1,17 @@
 import androidx.compose.ui.window.ComposeUIViewController
 import app.wesplit.App
 import app.wesplit.domain.model.AnalyticsManager
+import app.wesplit.domain.model.account.LoginDelegate
 import org.koin.dsl.module
+import platform.UIKit.UIViewController
 
-fun mainViewController() =
-    ComposeUIViewController {
+fun mainViewController(): UIViewController {
+    return ComposeUIViewController {
         App(
-            module { single<AnalyticsManager> { IosAnalyticsManager() } },
+            module {
+                single<AnalyticsManager> { IosAnalyticsManager() }
+                single<LoginDelegate> { LoginIosDelegate() }
+            },
         )
     }
+}
