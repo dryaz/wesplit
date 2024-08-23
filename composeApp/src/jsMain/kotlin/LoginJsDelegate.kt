d642a991d83e7b89aab71217a467cb7755b5b94c
Dmitrii Riazantsev <dilix90@gmail.com>
2024-08-24 00:57:25 +0200

    Try to add linking to anon

--- composeApp/src/jsMain/kotlin/LoginJsDelegate.kt
+++ composeApp/src/jsMain/kotlin/LoginJsDelegate.kt
@@ -11,8 +11,17 @@ class LoginJsDelegate : LoginDelegate {
         type: LoginType,
         onLogin: (Result<FirebaseUser>) -> Unit,
     ) {
+        // TODO: Gitlive yet not supported linkWithPopup for anon user, but worth to have it
         signInWithPopup(Firebase.auth.js, GoogleAuthProvider()).then { result ->
             val user = Firebase.auth.currentUser
+            val googleJsCreds = GoogleAuthProvider.credentialFromResult(result)
+            // Here we can't link acc 'cause acc already created, firebase could only link new non created acc
+            // There is linkWithPopup API in firebase JS but not yet implemented in Gitlibe
+            // TODO: PR to opensource gitlive to support link with
+//            val googleCreds = dev.gitlive.firebase.auth.GoogleAuthProvider.credential(
+//                idToken = googleJsCreds?.idToken,
+//                accessToken = googleJsCreds?.accessToken
+//            )
             if (user != null) {
                 onLogin(Result.success(user))
             } else {
