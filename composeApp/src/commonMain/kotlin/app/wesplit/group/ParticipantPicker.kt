f35b2bc46b6a4aa1e35aeca2f13e2c7b009d633c
Dmitrii Riazantsev <dilix90@gmail.com>
2024-09-04 17:37:25 +0200

    Participat picker improvements

--- composeApp/src/commonMain/kotlin/app/wesplit/group/ParticipantPicker.kt
+++ composeApp/src/commonMain/kotlin/app/wesplit/group/ParticipantPicker.kt
@@ -1,18 +1,30 @@
 package app.wesplit.group
 
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -20,7 +32,13 @@ import app.wesplit.domain.model.group.GroupRepository
 import app.wesplit.domain.model.group.Participant
 import app.wesplit.domain.model.user.ContactListDelegate
 import kotlinx.coroutines.Dispatchers
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 import org.koin.compose.koinInject
+import split.composeapp.generated.resources.Res
+import split.composeapp.generated.resources.img_search_empty
+import split.composeapp.generated.resources.no_contact_found
+import split.composeapp.generated.resources.search_contact
 
 // TODO: Ux improvement - add multiple users from single bottom sheet
 @OptIn(ExperimentalMaterial3Api::class)
@@ -52,17 +70,25 @@ internal fun ParticipantPicker(onUserSelect: (Participant?) -> Unit) {
     val lazyColumnListState = rememberLazyListState()
 
     // TODO: Min sheet size. Is it possible not to jump during filtering by text?
+    // TODO: (Idea) Add search to the bottom so even collapsing items won't affect users' UX
     ModalBottomSheet(
         onDismissRequest = { onUserSelect(null) },
     ) {
-        // TODO: Padding/styling
         OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(1f).padding(horizontal = 16.dp),
             value = searchText.value,
             onValueChange = { value -> viewModel.search(value) },
+            prefix = { Icon(Icons.Filled.Search, stringResource(Res.string.search_contact)) },
+            placeholder = {
+                Text(
+                    text = stringResource(Res.string.search_contact),
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+            },
         )
-        AnimatedVisibility(visible = participants.value != null) {
+        val anyParticipants = (participants.value?.size ?: 0) > 0
+        AnimatedVisibility(modifier = Modifier.fillMaxSize(1f), visible = anyParticipants) {
             LazyColumn(
-                modifier = Modifier.weight(1f),
                 state = lazyColumnListState,
             ) {
                 items(items = participants.value ?: emptyList(), key = { it.name }) { participant ->
@@ -73,7 +99,25 @@ internal fun ParticipantPicker(onUserSelect: (Participant?) -> Unit) {
                 }
             }
         }
+        AnimatedVisibility(modifier = Modifier.fillMaxSize(1f), visible = !anyParticipants) {
+            Column(
+                modifier = Modifier.padding(top = 32.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Image(
+                    modifier = Modifier,
+                    painter = painterResource(Res.drawable.img_search_empty),
+                    contentDescription = stringResource(Res.string.no_contact_found),
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+                Text(
+                    text = stringResource(Res.string.no_contact_found),
+                    style = MaterialTheme.typography.bodyLarge,
+                )
+            }
+        }
         // TODO: Loading
+        // TODO: Empty state
     }
 
     // TODO: Fire callback after modal is closed first to improve UX
