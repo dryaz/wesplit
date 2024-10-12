3672b94c4c7893fbabf67ef58b1205ab9502e789
Dmitrii Riazantsev <dilix90@gmail.com>
2024-10-12 22:28:25 +0200

    Request for review fix

--- composeApp/src/androidMain/kotlin/app/wesplit/AndroidAppReviewManager.kt
+++ composeApp/src/androidMain/kotlin/app/wesplit/AndroidAppReviewManager.kt
@@ -3,19 +3,32 @@ package app.wesplit
 import app.wesplit.di.ActivityProvider
 import app.wesplit.domain.model.AppReviewManager
 import app.wesplit.domain.model.ReviewType
+import com.mikhailovskii.inappreview.ReviewCode
 import com.mikhailovskii.inappreview.googlePlay.GooglePlayInAppReviewInitParams
 import com.mikhailovskii.inappreview.googlePlay.GooglePlayInAppReviewManager
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.mapLatest
 
 class AndroidAppReviewManager(
     private val activityProvider: ActivityProvider,
 ) : AppReviewManager {
-    override fun requestReview(type: ReviewType) {
-        activityProvider.activeActivity?.let {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override fun requestReview(type: ReviewType): Flow<Result<Unit>> {
+        return activityProvider.activeActivity?.let {
             val reviewManager = GooglePlayInAppReviewManager(GooglePlayInAppReviewInitParams(it))
             when (type) {
-                ReviewType.IN_APP -> reviewManager.requestInAppReview()
-                ReviewType.MARKET -> reviewManager.requestInMarketReview()
+                ReviewType.IN_APP -> reviewManager.requestInAppReview().mapLatest { code -> result(code) }
+                ReviewType.MARKET -> reviewManager.requestInMarketReview().mapLatest { code -> result(code) }
             }
-        }
+        } ?: flow { emit(Result.failure<Unit>(RuntimeException("Activity not found"))) }
     }
+
+    private fun result(code: ReviewCode) =
+        if (code == ReviewCode.NO_ERROR) {
+            Result.success(Unit)
+        } else {
+            Result.failure<Unit>(RuntimeException(code.name))
+        }
 }
--- composeApp/src/iosMain/kotlin/IosAppReviewManager.kt
+++ composeApp/src/iosMain/kotlin/IosAppReviewManager.kt
@@ -1,15 +1,26 @@
 import app.wesplit.domain.model.AppReviewManager
 import app.wesplit.domain.model.ReviewType
+import com.mikhailovskii.inappreview.ReviewCode
 import com.mikhailovskii.inappreview.appStore.AppStoreInAppReviewInitParams
 import com.mikhailovskii.inappreview.appStore.AppStoreInAppReviewManager
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.mapLatest
 
 class IosAppReviewManager : AppReviewManager {
     private val reviewManager = AppStoreInAppReviewManager(AppStoreInAppReviewInitParams("6714482007"))
 
-    override fun requestReview(type: ReviewType) {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    override fun requestReview(type: ReviewType): Flow<Result<Unit>> =
         when (type) {
-            ReviewType.IN_APP -> reviewManager.requestInAppReview()
-            ReviewType.MARKET -> reviewManager.requestInMarketReview()
+            ReviewType.IN_APP -> reviewManager.requestInAppReview().mapLatest { code -> result(code) }
+            ReviewType.MARKET -> reviewManager.requestInMarketReview().mapLatest { code -> result(code) }
         }
-    }
+
+    private fun result(code: ReviewCode) =
+        if (code == ReviewCode.NO_ERROR) {
+            Result.success(Unit)
+        } else {
+            Result.failure<Unit>(RuntimeException(code.name))
+        }
 }
