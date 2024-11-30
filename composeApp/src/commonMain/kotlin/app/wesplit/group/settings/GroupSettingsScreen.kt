69c43036d6d2db6677e8099e217635b99bf557d2
Dmitrii Riazantsev <dilix90@gmail.com>
2024-12-01 00:12:44 +0100

    Fix click zone on ios

--- composeApp/src/commonMain/kotlin/app/wesplit/group/settings/GroupSettingsScreen.kt
+++ composeApp/src/commonMain/kotlin/app/wesplit/group/settings/GroupSettingsScreen.kt
@@ -8,10 +8,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -600,7 +600,7 @@ private fun TopAppBareByState(
         actions = {
             Box(
                 modifier =
-                    Modifier.fillMaxHeight(1f).clickable {
+                    Modifier.heightIn(min = 52.dp).clickable {
                         when (dataState) {
                             is GroupSettingsViewModel.DataState.Error ->
                                 onToolbarAction(
